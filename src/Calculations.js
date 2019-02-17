db46fac40581cde4977fe8a391902ebd87862371
joshgrant <joshua.m.grant@gmail.com>
2019-02-17 16:41:49 -0500

    updated app and tests for starting state

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,37 +1,7 @@
 class FizzBuzzCalculations {
 
-  getFizzBuzz(int_value){
-    if ((int_value % 3 === 0) && (int_value % 5 === 0)) {
-      return "FizzBuzz";
-    }
-    else if (int_value % 5 === 0) {
-      return "Buzz";
-    }
-    else if (int_value % 3 === 0) {
-      return "Fizz"
-    }
-    else {
-      return parseInt(int_value, 10);
-    }
-  }
-
-  isInRange(int_value) {
-    return ((int_value > 0) && (int_value < 101))
-  }
-
-  getValue(input) {
-    const value = parseInt(input, 10);
-    if (Number.isInteger(value) && this.isInRange(value)) { return this.getFizzBuzz(value) }
-    else if (input === '') {
-      return "Waiting for input..."
-    }
-    else if (!Number.isInteger(value)) {
-      return "Error, not a number"
-    }
-    else if (!this.isInRange(value)){
-      return "Error, out of range"
-    }
-    else { return "Error, invalid input" }
+  getValue(input){
+    return "Waiting";
   }
 
 }
