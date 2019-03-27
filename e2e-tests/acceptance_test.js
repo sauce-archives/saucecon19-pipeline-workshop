82a8dcdc419c7faef8c6ddb0df8dd1fe02050d9d
joshgrant <joshua.m.grant@gmail.com>
2019-03-27 16:26:25 -0400

    updates to wdio v5

--- e2e-tests/acceptance_test.js
+++ e2e-tests/acceptance_test.js
@@ -5,10 +5,10 @@ describe("Basic App Functionality", () => {
     it('should show the correct value for 15', () => {
         browser.url('/index.html');
 
-        const input = browser.element('.fizzbuzzinput')
+        const input = $('.fizzbuzzinput')
         input.addValue('15')
 
-        const messageText = browser.element('.fizzBuzzMessage').getText();
+        const messageText = $('.fizzBuzzMessage').getText();
         const expectedText = "FizzBuzz"
 
         assert.equal(messageText, expectedText);
