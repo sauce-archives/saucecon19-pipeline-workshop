fcb8eac1b45b8ac8a4edb728769e3a9fe08a3a13
megha.angal <megha.angal@invitae.com>
2019-04-02 11:22:26 -0700

    Megha's change

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,9 +1,16 @@
 class FizzBuzzCalculations {
 
   getValue(int_value){
-      return "";
-  }
 
+     if (int_value%3 == 0) && *int_value%5 == 0)
+        return "fizzbuzz";
+     else if (int_value%5 == 0)
+        return "buzz";
+     else if (int_value%3 == 0)
+       return "fizz";
+     else
+       return "";
+  }
 }
 
 export default FizzBuzzCalculations;
