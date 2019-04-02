a40bf4d38a588ca72f4e1a11ac4c9d00ecee0e4a
eireland <eireland@concord.org>
2019-04-02 10:38:18 -0700

    initial commit of fizz buzz calc

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,7 +1,18 @@
 class FizzBuzzCalculations {
 
   getValue(int_value){
-      return "";
+    if ((int_value%3==0) && (int_value%5==0)) {
+      return "FizzBuzz";
+    }  
+    else if (int_value%5==0) {
+      return "Buzzy";
+    }
+    else if (int_value%3==0) {
+      return "Fizzy!"
+    }
+    else {
+      return "please enter a valid number"
+    }
   }
 
 }
