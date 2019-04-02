fb437a6302a9d2ad09911962d5cdd56998063271
megha.angal <megha.angal@invitae.com>
2019-04-02 11:28:23 -0700

    Megha's changei again

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,15 +1,7 @@
 class FizzBuzzCalculations {
 
   getValue(int_value){
-
-     if (int_value%3 == 0) && *int_value%5 == 0)
-        return "fizzbuzz";
-     else if (int_value%5 == 0)
-        return "buzz";
-     else if (int_value%3 == 0)
-       return "fizz";
-     else
-       return "";
+       return "fizz and buzz and fizzbuzz";
   }
 }
 
