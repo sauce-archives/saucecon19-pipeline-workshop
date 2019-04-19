d6a08467db1241894c212d151d7dc1a4fdf525f4
Matthew Keenum <matthew.keenum@regions.com>
2019-04-19 14:56:26 -0500

    Challenge 1 and 2 completed

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,9 +1,25 @@
 class FizzBuzzCalculations {
+    getValue(input){
+        let result = "";
 
-  getValue(int_value){
-      return "";
-  }
+        if (input > 101 || < 1){
+            result = "out of range";
+        }
+        else if (input % 15 === 0){
+            result = "FizzBuzz";
+        }
+        else if (input % 3 === 0){
+            result = "Fizz";
+        }
+        else if (input % 5 === 0){
+            result = "Buzz";
+        }
+        else{
+            result = input
+        }
 
+        return result;
+   }
 }
 
-export default FizzBuzzCalculations;
+export default FizzBuzzCalculations
