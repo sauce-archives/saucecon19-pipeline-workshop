ead0d8eace27cb68ebb16d4b91bbe17f8290fe9a
Austin_Mathias <Austin_Mathias@W10FBVKH72>
2019-04-23 16:48:04 -0500

    exercise 2

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,17 +1,21 @@
 class FizzBuzzCalculations {
 
   getValue(int_value){
-    if (int_value % 3 === 0 && int_value % 5 === 0){
-      return "FizzBuzz";
-     }
-    if (int_value % 3 === 0){
-      return "Fizz";
-     }
-    if (int_value % 5 === 0){
-      return "Buzz";
-     }
+    let result = "";
+    if (input % 15 === 0){
+      result = "FizzBuzz";
+    }
+    else if (input % 5 === 0) {
+      result = "Buzz";
+    }
+    else if (input % 3 === 0){
+      result = "Fizz";
+    }
+    if (input > 100 || input < 1) {
+      result = "out of range"
+    }
+    return result;
   }
-
 }
 
 export default FizzBuzzCalculations;
