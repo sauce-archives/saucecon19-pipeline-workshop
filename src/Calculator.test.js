98f304d8b3faba745b90e561379932f6d2d9589c
joshgrant <joshua.m.grant@gmail.com>
2019-04-23 16:01:21 -0500

    added unit test

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -3,5 +3,10 @@ import FizzBuzzCalculations from "./Calculations"
 const assert = require('assert');
 
 it('calculates Fizz correctly', () => {
-  
+  calc = new FizzBuzzCalculations();
+
+  actual = calc.getValue(3);
+  expected = "Fizz"
+
+  assert.equal(actual, expected);
 });
