3138682aa2289750c3086b2e1d0173be04924832
Corneliu Duplachi <corneliu.duplachi@ellation.com>
2019-04-23 15:51:57 -0500

    Add implementation

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,9 +1,15 @@
 class FizzBuzzCalculations {
-
-  getValue(int_value){
-      return "";
-  }
-
+    getValue(input) {
+        // if (input > 0 && input < 101)
+            if (input % 15 === 0)
+                return 'FizzBuzz';
+            else if (input % 3 === 0)
+                return 'Fizz';
+            else
+                return input % 5 === 0 ? 'Buzz' : '';
+        // else
+        //     return 'out of range'
+    }
 }
 
 export default FizzBuzzCalculations;
