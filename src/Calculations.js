2026b2c7876f193ecaf1f69c0b45d12149e801f8
jorgem <jorge@wowza.com>
2019-04-23 15:48:57 -0500

    Calculate fizzbuzz and unit tests

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,5 +1,6 @@
 class FizzBuzzCalculations {
 
+<<<<<<< Updated upstream
   getValue(int_value){
       if (int_value % 5 === 0){
 	  return "Buzz"
@@ -11,6 +12,29 @@ class FizzBuzzCalculations {
 	  return "FizzBuzz"
       }
      
+=======
+  getValue(input){
+     var  modby3 = false;
+     var  modby5 = false;
+
+     if (int_value % 3 === 0){
+         modby3 = true;
+     }
+     if (int_value % 5 == 0) {
+         modby5 = true;
+     }
+     if (modby3 && modby5){
+       return "FizzBuzz"
+     }else if (modby3 ){
+        return "Fizz"
+
+     }else if (modby5){
+        return "Buzz"
+     }else {
+        return "BAD"
+     }
+
+>>>>>>> Stashed changes
   }
 
 }
