f4ac7d08b5bec08756a038f202335e2e62259fbb
Trent Walkup <trentonwalkup@legalshieldcorp.com>
2019-04-23 16:08:33 -0500

    TrentonWalkup test PR

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,9 +1,20 @@
 class FizzBuzzCalculations {
 
-  getValue(int_value){
-      return "";
+  getValue(input){
+    let result = ""
+    if (input % 3 === 0){
+      result = "Fizz"
+    } else if (input % 5 === 0){
+      result = "Buzz"
+    } else if (input % 15 === 0){
+      result = "FizzBuzz"
+    }
+      else if (input < 101 && input > 0){
+        result = input
+      } else {
+        result = "out of range"
+      }
+    return result;
   }
-
 }
-
 export default FizzBuzzCalculations;
--- wdio.conf.js
+++ wdio.conf.js
@@ -15,15 +15,15 @@ exports.config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
+    // user: process.env.SAUCE_USERNAME,
+    // key: process.env.SAUCE_ACCESS_KEY,
     //
     // If you run your tests on SauceLabs you can specify the region you want to run your tests
     // in via the `region` property. Available short handles for regions are `us` (default) and `eu`.
     // These regions are used for the Sauce Labs VM cloud and the Sauce Labs Real Device Cloud.
     // If you don't provide the region it will default for the `us`
-    
-    
+
+
     //
     // ==================
     // Specify Test Files
@@ -71,7 +71,7 @@ exports.config = {
       {browserName: 'firefox', platform: 'Windows 10', version: 'latest'},
       {browserName: 'chrome', platform: 'OS X 10.13', version: 'latest', 'extendedDebugging': true},
     ],
-    
+
     // use built-in Sauce Connect functionality included with wdio-sauce-service
     sauceConnect: true,
     region: 'us',
@@ -140,7 +140,7 @@ exports.config = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
     reporters: ['dot','spec'],
-    
+
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
@@ -187,7 +187,7 @@ exports.config = {
      */
     // beforeCommand: function (commandName, args) {
     // },
-    
+
     /**
      * Hook that gets executed before the suite starts
      * @param {Object} suite suite details
@@ -224,7 +224,7 @@ exports.config = {
      */
     // afterSuite: function (suite) {
     // },
-    
+
     /**
      * Runs after a WebdriverIO command gets executed
      * @param {String} commandName hook command name
